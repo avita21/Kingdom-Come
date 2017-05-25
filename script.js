18173ef91ce1eed126078577b564e5ef9c717071
avita21 <avita.sharma@tufts.edu>
2017-05-25 19:25:48 -0400

    added transition to nvl

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 
 /* Fade out title on click */
 $(document).ready(function(){
-	$(".kingdomcome").click(function(){
+	$(".kingdomcome").one('click', function(){
 		$("#back").fadeOut(1000, function() {
 			$(this).css('background', 'linear-gradient(to right, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 30%,rgba(0,0,0,0) 70%,rgba(0,0,0,1) 100%').fadeIn('3000');
 			$(".kingdomcome").fadeOut(1000, function(){
-				$(".kingdomtitle").append("Kingdom Come");
+				$(".kingdomtitle").append("Kingdom Come<hr>");
 				$(".kingdomtitle").fadeIn(1000);
+				//$("body").append("<hr>");
 			});
-
 		});
 	});
 });
