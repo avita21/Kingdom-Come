53e12a1b791f06bc165f4917d97a87c638bc2eb0
avita21 <avita.sharma@tufts.edu>
2017-05-27 10:55:40 -0400

    reduced shadow on border, started xml

--- script.js
+++ script.js
@@ -5,7 +5,8 @@
 $(document).ready(function(){
 	$(".kingdomcome").one('click', function(){
 		$("#back").fadeOut(1000, function() {
-			$(this).css('background', 'linear-gradient(to right, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 30%,rgba(0,0,0,0) 70%,rgba(0,0,0,1) 100%').fadeIn('3000');
+			$(this).css('background', 
+			'linear-gradient(to right, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 10%,rgba(0,0,0,0) 90%,rgba(0,0,0,1) 100%').fadeIn('3000');
 			$(".kingdomcome").fadeOut(1000, function(){
 				$(".kingdomtitle").append("Kingdom Come<hr>");
 				$(".kingdomtitle").fadeIn(1000);
