4a2d7b0c1a98c4df056d7956fda7b401b0321940
avita21 <avita.sharma@tufts.edu>
2017-06-11 15:16:21 -0400

    added xml

--- script.js
+++ script.js
@@ -6,12 +6,44 @@ $(document).ready(function(){
 	$(".kingdomcome").one('click', function(){
 		$("#back").fadeOut(1000, function() {
 			$(this).css('background', 
-			'linear-gradient(to right, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 10%,rgba(0,0,0,0) 90%,rgba(0,0,0,1) 100%').fadeIn('3000');
+			'linear-gradient(to right, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 20%,rgba(0,0,0,0) 80%,rgba(0,0,0,1) 100%').fadeIn('3000');
 			$(".kingdomcome").fadeOut(1000, function(){
 				$(".kingdomtitle").append("Kingdom Come<hr>");
 				$(".kingdomtitle").fadeIn(1000);
-				//$("body").append("<hr>");
+				// BEGIN THE GAME:
+				kingdom_come();
 			});
 		});
 	});
 });
+
+
+/* 
+   ######################################################
+		BEGINNING THE NARRATIVE - XML PARSING
+	####################################################
+*/
+
+function kingdom_come(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	        begin_narrative(this);
+	    }
+	};
+	xhttp.open("GET", "KingdomCome.xml", true);
+	xhttp.send();
+}
+
+function begin_narrative(xml) {
+	xmlDoc = xml.responseXML;
+	var x, i, txt;
+	txt = "";
+    x = xmlDoc.documentElement.childNodes;
+    for (i = 0; i < x.length; i++) { 
+        if (x[i].nodeType == 1) {
+            txt += x[i].nodeName + "<br>";
+        }
+    }
+	document.getElementById("narrative").innerHTML = txt;
+}
