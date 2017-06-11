59ed1f54edcc350ce54302ad8e6c0a00f88eab75
avita21 <avita.sharma@tufts.edu>
2017-06-11 16:53:01 -0400

    TRYING TO MAKE IT WORK FOR MOBILE >:(

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 
 /* Fade out title on click */
 $(document).ready(function(){
-	/* Detect if on mobile device: */
-	var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+	/* Detect if on desktop: */
+	var isDesktop = window.matchMedia("only screen and (min-width: 600px)");
 	$(".kingdomcome").one('click', function(){
 		$("#back").fadeOut(1000, function() {
 			/* add faded black background if not on mobile: */
-			if (!isMobile.matches) {
+			if (isDesktop.matches) {
 				$(this).css('background', 
 				'linear-gradient(to right, rgba(0,0,0,1) 0%,rgba(0,0,0,0) 20%,rgba(0,0,0,0) 80%,rgba(0,0,0,1) 100%').fadeIn('3000');
 			}
